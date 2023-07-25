17a4735ed209fdffa1aa112f8a7c5316e924fb5f
Jagan-dev2 <jagan.sanivarapu+d1@gmail.com>
2023-07-25 11:00:20 +0530

    added firstPipe

---
+++ vars/firstPipelibrary.groovy
@@ -0,0 +1,22 @@
+import com.jagandev.builds.Calculator
+def call(Map PipelineParams){
+    Calculator calculator = new Calculator(this)
+    pipeline{
+    agent any
+    environment {
+        APP_NAME = "${PipelineParams.appName}"
+    }
+    stages{
+        stage('echo')
+        {
+            steps{
+                script{
+                    echo "Implementing library"
+                    println calculator.add(3,4)
+                    echo "MIcroservice name is ${APP_NAME}"
+                }
+            }
+        }
+    }
+}
+}
